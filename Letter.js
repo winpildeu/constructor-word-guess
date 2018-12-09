6bb2725042991eb10b8ad63237aa1179e865baa8
Winfield H <winterfreshness@gmail.com>
2018-12-09 16:09:40 -0500

    Letter.js works

---
+++ Letter.js
@@ -0,0 +1,24 @@
+console.log(`Letter.js loaded...\n`);
+
+function Letter (char) {
+    // the letter that needs to be guessed
+    this.letter = char,
+    // T/F value if the letter has been correctly guessed or not
+    this.guessed = false;
+    // func. that displays the real letter if guessed = true, shows '_' otherwise
+    this.display = function() {
+        (this.guessed === true) ? this.letter : '_'
+    },
+    // takes in the user's guess and checks if it and the hidden letter match
+    this.check = function(charGuess) {
+        (charGuess === this.letter) ? this.guessed = true : null
+    }
+}
+
+// object test
+// let char = new Letter("v");
+// console.log(char);
+// char.display();
+// char.check("w");
+
+module.exports = Letter;
---
+++ Word.js
@@ -0,0 +1,5 @@
+console.log(`Word.js loaded...\n`);
+
+let Letter = require('Letter');
+
+module.exports = Letter;
