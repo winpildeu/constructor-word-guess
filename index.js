e69ca8f8c75a6def3c447a79a9e053ab71dc5f48
Winfield H <winterfreshness@gmail.com>
2018-12-10 17:42:17 -0500

    Added: imports wordBank file, makes array and randomly chooses a word.

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-let inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const Word = require('./Word');
+const fs = require('fs');
+let hiddenWord;
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  });
+// import the word bank file
+fs.readFile("wordBank.txt", "utf8", (err, data) => {
+  (err) ? console.log(err): null
+  const wordBank = data.split(',');
+  console.log(wordBank);
+
+  // choose a random index in the word bank and set it to the hidden word
+  hiddenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  console.log(`\nHidden word is: ${hiddenWord}`);
+});
+
+
+// const x = "guess";
+// console.log(`Word: ${x}`);
+
+// let array = x.split('');
+// console.log(`Array: ${array}`);
+
+// let word = new Word();
+// array.forEach(element => {
+//     word.wordArray.push(new Letter(element));
+// });
+// console.log(`wordArray: ${word.wordArray}`);
+
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then(answers => {
+//     // Use user feedback for... whatever!!
+//   });
